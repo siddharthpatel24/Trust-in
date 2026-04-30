4ae74ea17eb98d7bb5ce20dfa0b8373fa1a0c46e
pulishetti Siddhartha <pulishettisiddharth@gmail.com>
2026-04-30 11:00:49 +0530

    Updated config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,19 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyDnVA5ZcwbzXtaTN8s8u4jgrF-vvz3cQX4",
-  // authDomain: "trust-in-3dcee.firebaseapp.com",
-  // projectId: "trust-in-3dcee",
-  // storageBucket: "trust-in-3dcee.firebasestorage.app",
-  // messagingSenderId: "200400973666",
-  // appId: "1:200400973666:web:3776d4a27a251b5ba5463b"
-  apiKey: "AIzaSyDranJuEY3nGiMtGCnfygdNUDnKRkh5tUI",
-  authDomain: "task-tracker-e3a15.firebaseapp.com",
-  projectId: "task-tracker-e3a15",
-  storageBucket: "task-tracker-e3a15.firebasestorage.app",
-  messagingSenderId: "791283701471",
-  appId: "1:791283701471:web:f2ffcfd0c67b9774aa3b6a",
-  measurementId: "G-HF38EGH52B"
+  apiKey: "AIzaSyDnVA5ZcwbzXtaTN8s8u4jgrF-vvz3cQX4",
+  authDomain: "trust-in-3dcee.firebaseapp.com",
+  projectId: "trust-in-3dcee",
+  storageBucket: "trust-in-3dcee.firebasestorage.app",
+  messagingSenderId: "200400973666",
+  appId: "1:200400973666:web:3776d4a27a251b5ba5463b"
 };
 
 const app = initializeApp(firebaseConfig);
