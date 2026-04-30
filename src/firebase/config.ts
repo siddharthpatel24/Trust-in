0c1762fec02c736c6faf75d9d460ca4063c875b7
pulishetti Siddhartha <pulishettisiddharth@gmail.com>
2026-04-30 10:59:06 +0530

    Updated config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnVA5ZcwbzXtaTN8s8u4jgrF-vvz3cQX4",
-  authDomain: "trust-in-3dcee.firebaseapp.com",
-  projectId: "trust-in-3dcee",
-  storageBucket: "trust-in-3dcee.firebasestorage.app",
-  messagingSenderId: "200400973666",
-  appId: "1:200400973666:web:3776d4a27a251b5ba5463b"
-apiKey: "AIzaSyDranJuEY3nGiMtGCnfygdNUDnKRkh5tUI",
+  // apiKey: "AIzaSyDnVA5ZcwbzXtaTN8s8u4jgrF-vvz3cQX4",
+  // authDomain: "trust-in-3dcee.firebaseapp.com",
+  // projectId: "trust-in-3dcee",
+  // storageBucket: "trust-in-3dcee.firebasestorage.app",
+  // messagingSenderId: "200400973666",
+  // appId: "1:200400973666:web:3776d4a27a251b5ba5463b"
+  apiKey: "AIzaSyDranJuEY3nGiMtGCnfygdNUDnKRkh5tUI",
   authDomain: "task-tracker-e3a15.firebaseapp.com",
   projectId: "task-tracker-e3a15",
   storageBucket: "task-tracker-e3a15.firebasestorage.app",
